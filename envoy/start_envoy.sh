d862ace1c3b402e8f0f8d48f9ef0d3feb24d227e
daviddelriod <delrioddavid@gmail.com>
2023-04-24 23:23:54 +0200

    Changes in the fqdn

--- envoy/start_envoy.sh
+++ envoy/start_envoy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-fx envoy start -n env_one --disable-tls --envoy-config-path envoy_config.yaml -dh ubuntu -dp 50051
+fx envoy start -n env_one --disable-tls --envoy-config-path envoy_config.yaml -dh 172.31.0.197 -dp 50051
--- envoy/start_envoy2.sh
+++ envoy/start_envoy2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-fx envoy start -n env_two --disable-tls --envoy-config-path envoy_config2.yaml -dh ubuntu -dp 50051
+fx envoy start -n env_two --disable-tls --envoy-config-path envoy_config2.yaml -dh 172.31.0.197 -dp 50051
