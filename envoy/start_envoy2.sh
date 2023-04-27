aa5b9673e19dcc5111a8baae7de5ac232b404770
David Del Rio <delrioddavid@gmail.com>
2023-04-27 14:19:27 +0000

    Ports changed

--- envoy/start_envoy2.sh
+++ envoy/start_envoy2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-fx envoy start -n env_two --disable-tls --envoy-config-path envoy_config2.yaml -dh ec2-52-20-29-134.compute-1.amazonaws.com -dp 22
+fx envoy start -n env_two --disable-tls --envoy-config-path envoy_config2.yaml -dh ec2-52-20-29-134.compute-1.amazonaws.com -dp 1024
